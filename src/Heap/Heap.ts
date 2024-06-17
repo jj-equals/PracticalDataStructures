cd14df1b9700b569d7efb24f97fd6a90ccf25305
jj-equals <jario.junior@equals.com.br>
2024-06-17 18:06:56 -0300

    chore: Finalizando a implantação de Heap como Priority Queue

--- src/Heap/Heap.ts
+++ src/Heap/Heap.ts
@@ -26,9 +26,52 @@ export class Heap<T> {
         [this.heapArray[index1], this.heapArray[index2]] = [this.heapArray[index2], this.heapArray[index1]];
     }
 
-
     public insert(value: T): void {
         this.heapArray.push(value);
-        let len = this.heapArray.length;
+        let current = this.heapArray.length - 1;
+
+        while (current > 0 && this.heapArray[current] > this.heapArray[this.parent(current)]) {
+            this.swap(current, this.parent(current));
+            current = this.parent(current);
+        }
+    }
+
+    private sinkDown(index: number): void {
+        let maxIndex = index;
+        let size = this.heapArray.length;
+
+        while (true) {
+            let leftIndex = this.leftChild(index);
+            let rightIndex = this.rightChild(index);
+
+            if (leftIndex < size && this.heapArray[leftIndex] > this.heapArray[maxIndex]) {
+                maxIndex = leftIndex;
+            }
+            if (rightIndex < size && this.heapArray[rightIndex] > this.heapArray[maxIndex]) {
+                maxIndex = rightIndex;
+            }
+            if (maxIndex !== index) {
+                this.swap(index, maxIndex);
+                index = maxIndex;
+            }
+            else {
+                return;
+            }
+        }
+    }
+
+    public remove(): T | undefined {
+        if (this.heapArray.length === 0) {
+            return undefined;
+        }
+        if (this.heapArray.length === 1) {
+            return this.heapArray.pop();
+        }
+
+        const maxValue = this.heapArray[0];
+        this.heapArray[0] = this.heapArray.pop() as T;
+
+        this.sinkDown(0);
+        return maxValue;
     }
 }
